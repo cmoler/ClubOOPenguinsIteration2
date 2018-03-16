aebbca997fb3e191a29dfe7ce0dcf1689a4310b0
Colton <coltonmoler@gmail.com>
2018-03-15 20:55:21 -0400

    for some reason these didnt push

--- src/View/AreaView/WorldView.java
+++ src/View/AreaView/WorldView.java
@@ -1,4 +1,29 @@
 package View.AreaView;
 
-public class WorldView {
+import Model.Map.Map;
+import Model.Map.World;
+import View.Viewport;
+
+import java.awt.*;
+import java.util.HashMap;
+
+public class WorldView extends Viewport{
+
+    private HashMap<Map,MapView> mapViews;
+    private MapView currentView;
+
+    public WorldView(HashMap<Map,MapView> mapViews){
+        this.mapViews = mapViews;
+    }
+
+    @Override
+    public void draw(Graphics2D graphics2D){
+        currentView.draw(graphics2D);
+    }
+
+    @Override
+    public void update(){
+        currentView = mapViews.get(World.getWorld().getCurrentMap());
+    }
+
 }
