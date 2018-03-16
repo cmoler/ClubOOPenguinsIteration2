519d5ea56fc12c7844d1dc5732085458c6491979
zscrib0619 <zscrib0619@ufl.edu>
2018-03-15 23:18:10 -0400

    Traps

--- src/Model/Entity/Role/Sneak.java
+++ src/Model/Entity/Role/Sneak.java
@@ -5,9 +5,14 @@ import Model.Entity.Skill.Creep;
 import Model.Entity.Skill.DetectAndRemoveTrap;
 import Model.Entity.Skill.PickPocket;
 import Model.Entity.Skill.RangedWeapon;
+import Model.Map.AreaEffect.AreaEffect;
 import Model.Map.AreaEffect.AreaEffectType;
+import Model.Map.AreaEffect.Trap;
 import Model.Map.Direction;
 import Model.Map.Location;
+
+import java.awt.geom.Area;
+import java.util.Random;
 
 public class Sneak extends Role {
 
@@ -59,11 +64,16 @@ public class Sneak extends Role {
     @Override
     public void activateTrait(Location location){
 
+
+
         //Detect Traps
+        int detectChance = getDetectAndRemoveTrap()*10;
+
         for (Direction dir : Direction.values()){
             Location adjLocation = location.getAdjacentAt(dir);
-            if (adjLocation.getAreaEffect().getAreaEffectType() ==  AreaEffectType.TRAP){
-                //TODO Setvisible based on level
+            AreaEffect areaEffect = adjLocation.getAreaEffect();
+            if (areaEffect.getAreaEffectType() ==  AreaEffectType.TRAP){
+                ((Trap) areaEffect).setVisible(detectChance);
             }
         }
 
--- src/Model/Map/AreaEffect/Trap.java
+++ src/Model/Map/AreaEffect/Trap.java
@@ -2,12 +2,22 @@ package Model.Map.AreaEffect;
 
 import Model.Entity.Entity;
 
+import java.util.Random;
+
 public class Trap extends OneShotAreaEffect{
     boolean isVisible;
 
 
-    public void setVisible(boolean visible) {
-        isVisible = visible;
+    public Trap() {
+        isVisible = false;
+    }
+
+    public void setVisible(int detectChance) {
+        Random random = new Random();
+        int chance = random.nextInt(100);
+        if (chance < detectChance ) {
+            isVisible = true;
+        }
     }
 
     public boolean isVisible() {
