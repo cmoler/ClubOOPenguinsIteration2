8badc8150bb6be861d27b5353bda157f8c831966
michaelflorica <floricamichael@gmail.com>
2018-03-17 16:13:45 -0400

    add Deserializer, refactor GameLoader

--- src/Controller/SavingLoading/GameLoader.java
+++ src/Controller/SavingLoading/GameLoader.java
@@ -12,97 +12,42 @@ import java.util.Scanner;
 
 public class GameLoader {
 
-    private World world;
+    private Deserializer deserializer;
+
+    private World world = World.getWorld();
     private Player player;
 
-    public void loadGame(String filePath) throws FileNotFoundException {
+    public GameLoader(String filePath) throws FileNotFoundException{
+        try{
 
-        JSONObject jsonGameContent = loadGameContentJSON(filePath);
-        loadPlayer(jsonGameContent);
-        loadWorld(jsonGameContent);
+            deserializer = new Deserializer(load(filePath));
+
+            //world is a singleton, so deserializer can access world
+            deserializer.deserializeWorld();
+
+            //deserialize gives back a Player object
+            player = deserializer.deserializePlayer();
+
+        } catch (FileNotFoundException fileNotFoundException){
+            throw fileNotFoundException;
+        }
     }
 
-    private JSONObject loadGameContentJSON(String filePath) throws FileNotFoundException{
+    public Player getPlayer(){
+        return player;
+    }
 
+    private JSONObject load(String filePath) throws FileNotFoundException {
         try {
+
             File saveFile = new File(filePath);
             String saveFileContent = new Scanner(saveFile).useDelimiter("\\Z").next();
+            JSONObject saveFileJSON = new JSONObject(saveFileContent);
 
-            JSONObject gameContentJSON = new JSONObject(saveFileContent);
-
-            return  gameContentJSON;
+            return  saveFileJSON;
 
         } catch (FileNotFoundException fileNotFoundException) {
             throw fileNotFoundException;
         }
-
-    }
-
-    private void loadWorld(JSONObject worldJSON){
-        world = World.getWorld();
-
-
-    }
-
-    private void loadPlayer(JSONObject jsonGameContent){
-
-        JSONObject playerJSON = jsonGameContent.getJSONObject("Player");
-
-        Inventory inventory = loadInventory(playerJSON.getJSONObject("Inventory"));
-        Equipment equipment = loadEquipment(playerJSON.getJSONObject("Equipment"));
-        Location   location = loadLocation(playerJSON.getJSONObject("Location"));
-        Role           role = loadRole(playerJSON.getJSONObject("Class"));
-
-        int level = loadLevel(playerJSON);
-        int HP    = loadHP(playerJSON);
-        int maxHP = loadMaxHP(playerJSON);
-        int mana  = loadMana(jsonGameContent);
-        int XP    = loadXP(jsonGameContent);
-        int gold  = loadGold(jsonGameContent);
-
-
-        //this.player = new Player(role, )
-    }
-
-    private Role loadRole(JSONObject role){
-
-    }
-
-
-
-    private Location loadLocation(JSONObject location){
-
-    }
-
-    private Inventory loadInventory(JSONObject inventory){
-
-    }
-
-    private Equipment loadEquipment(JSONObject equipment){
-
-    }
-
-    private int loadLevel(JSONObject playerJSON){
-        int level = playerJSON.getInt("Level");
-    }
-
-    private int loadHP(JSONObject playerJSON){
-        int HP  = playerJSON.getInt("HP");
-    }
-
-    private int loadMaxHP(JSONObject playerJSON){
-        int maxXP = playerJSON.getInt("MaxHP");
-    }
-
-    private int loadMana(JSONObject playerJSON){
-        int mana = playerJSON.getInt("Mana");
-    }
-
-    private int loadXP(JSONObject playerJSON){
-        int XP = playerJSON.getInt("XP");
-    }
-
-    private int loadGold(JSONObject playerJSON){
-        int gold = playerJSON.getInt("Gold");
     }
 }
