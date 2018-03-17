d2e93eb909884d518d42ae9b5ad87774e168a7b5
Keanu Kerr <keanukerr@gmail.com>
2018-03-17 18:22:20 -0400

    Merged master with view

--- src/Model/Entity/Skill/Observation.java
+++ src/Model/Entity/Skill/Observation.java
@@ -1,8 +1,20 @@
 package Model.Entity.Skill;
 
-public class Observation extends Skill {
+import Model.Map.Location;
 
-    public Observation(int points){
-        super(points);
-    }
+import java.util.ArrayList;
+
+public class Observation extends Skill {
+//    public Observation(int points){
+//        super(points);
+//    }
+//
+//    public String getTileItems(Location location) {
+//        Location.LocationItemIterator li = new Location.LocationItemIterator();
+//        ArrayList<String> itemArr = new ArrayList<String>();
+//    }
+//
+//    public String getTileType(Location locatoin) {
+//
+//    }
 }
