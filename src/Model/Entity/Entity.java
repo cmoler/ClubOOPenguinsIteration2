725cfbad024b2ba30bbafa179d753e76392da277
Colton <coltonmoler@gmail.com>
2018-03-18 15:14:46 -0400

    added default direction

--- src/Model/Entity/Entity.java
+++ src/Model/Entity/Entity.java
@@ -22,7 +22,7 @@ public abstract class Entity {
     private EntityType entityType;
     private int maxHealth = 100;
     private int health = maxHealth;
-    private Direction directionFacing;
+    private Direction directionFacing = Direction.N;
     private int experience = 0;
     private int level = 1; // default level
     private double defense = 0;
