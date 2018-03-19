a8dd23ae6481f8fb61de41a0dfbdad8d9486b6bb
zscrib0619 <zscrib0619@ufl.edu>
2018-03-19 01:12:24 -0400

    Traps

--- src/Model/Map/AreaEffect/TrapAreaEffect.java
+++ src/Model/Map/AreaEffect/TrapAreaEffect.java
@@ -22,8 +22,10 @@ public class TrapAreaEffect extends OneShotAreaEffect{
         int chance = random.nextInt(100);
         if (chance < detectChance ) {
             isVisible = true;
+            notifyView();
         }
     }
+
 
     public boolean isVisible() {
         return isVisible;
@@ -36,6 +38,7 @@ public class TrapAreaEffect extends OneShotAreaEffect{
     @Override
     protected void affect(Entity entity) {
         entity.takeDamage(entity.getMaxHealth()/2);
+        isVisible = true;
         setActive(false);
         notifyView();
     }
@@ -47,6 +50,10 @@ public class TrapAreaEffect extends OneShotAreaEffect{
 
     public void attach(TrapView trapView) {
         observers.add(trapView);
+    }
+
+    public void detach(Viewport viewport){
+        observers.remove(viewport);
     }
 
     public void notifyView(){
