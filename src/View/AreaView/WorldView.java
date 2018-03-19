43ff527cefb6ac6cfe640977825942816f31c636
Colton <coltonmoler@gmail.com>
2018-03-18 22:23:58 -0400

    update

--- src/View/AreaView/WorldView.java
+++ src/View/AreaView/WorldView.java
@@ -14,18 +14,13 @@ public class WorldView extends Viewport{
 
     public WorldView(HashMap<Map,MapView> mapViews){
         this.mapViews = mapViews;
-        update();
     }
 
     @Override
     public void draw(Graphics2D graphics2D){
+        currentView = mapViews.get(World.getWorld().getCurrentMap());
         if(currentView != null) currentView.draw(graphics2D);
         else update();
-    }
-
-    @Override
-    public void update(){
-        currentView = mapViews.get(World.getWorld().getCurrentMap());
     }
 
 }
